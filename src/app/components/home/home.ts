72fea5be42d19fb57933b3109a8e0728e4a116c7
Alexandre Ballardin <aleballa08@gmail.com>
2026-02-16 09:26:52 -0300

    btn component

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { Header } from '../header/header';
 import { NgOptimizedImage } from '@angular/common';
+import { BtnPrimary } from '../btn-primary/btn-primary';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, NgOptimizedImage],
+  imports: [Header, NgOptimizedImage, BtnPrimary],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
-
+  callWhatsapp() {
+    window.location.href = "https://wa.me/555432914810?text=Ol%C3%A1!%20Venho%20do%20site%20e%20gostaria%20de%20saber%20mais...";
+  }
 }
